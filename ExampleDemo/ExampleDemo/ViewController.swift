622c2a1a6b22714ef4cc04566704818c703c73ea
13166253532 <13166253532@163.com>
2017-11-30 15:19:13 +0800

    初始化

---
+++ ExampleDemo/ExampleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleDemo
+//
+//  Created by xiejiangbo on 2017/11/30.
+//  Copyright © 2017年 yin chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
