064667efbe27762b14ff82ba3d12b0b8e2f322d9
13166253532 <13166253532@163.com>
2018-07-25 18:05:37 +0800

    model 排序

--- ExampleDemo/ExampleDemoTests/DSHttpMessageListCmdText.swift
+++ ExampleDemo/ExampleDemoTests/DSHttpMessageListCmdText.swift
@@ -23,28 +23,57 @@ class DSHttpMessageListCmdText: XCTestCase {
     }
     
     func testDSHttpMessageListCmd() {
-        let completeDelegate:SMCommandComplete = SMCommandComplete.init()
-        let cmd:HttpCommand = DSHttpMessageListCmd.init(version: PHttpVersion_v1)
+//        let completeDelegate:SMCommandComplete = SMCommandComplete.init()
+//        let cmd:HttpCommand = DSHttpMessageListCmd.init(version: PHttpVersion_v1)
+//
+//        let dic:NSMutableDictionary = NSMutableDictionary()
+//        dic[kHttpParamKey_PerInvestmentList_sortType] = "DESC"
+//        dic[kHttpParamKey_PerInvestmentList_sortItem] = "followCount"
+//
+//        cmd.requestInfo = dic as [NSObject:AnyObject]
+//
+//        cmd.completeDelegate = completeDelegate
+//
+//        print("url == %@",cmd.getUrl())
+//        cmd.execute()
+//        completeDelegate.waitForComplete()
+//        XCTAssert(cmd.result.isOk())
+//        let r:DSHttpMessageListResult = cmd.result as! DSHttpMessageListResult
+//        if r.isOk() {
+//            print("success")
+//        }else{
+//            print("failed")
+//        }
+        let array = NSMutableArray()
+        for index in 0..<10 {
+            let model = CompanyListInfoMode()
+            model.org_id = String(8+index)
+            model.org_name = String(100-index)
+            array.add(model)
+        }
+        self.sequencePerTime(arr: array)
         
-        let dic:NSMutableDictionary = NSMutableDictionary()
-        dic[kHttpParamKey_PerInvestmentList_sortType] = "DESC"
-        dic[kHttpParamKey_PerInvestmentList_sortItem] = "followCount"
-        
-        cmd.requestInfo = dic as [NSObject:AnyObject]
-        
-        cmd.completeDelegate = completeDelegate
-        
-        print("url == %@",cmd.getUrl())
-        cmd.execute()
-        completeDelegate.waitForComplete()
-        XCTAssert(cmd.result.isOk())
-        let r:DSHttpMessageListResult = cmd.result as! DSHttpMessageListResult
-        if r.isOk() {
-            print("success")
-        }else{
-            print("failed")
+    }
+    func sequencePerTime(arr:NSMutableArray)  {
+        let list = arr.sortedArray { (infor1, infor2) -> ComparisonResult in
+            let infor3 = infor1 as! CompanyListInfoMode
+            let infor4 = infor2 as! CompanyListInfoMode
+            let org_id3:Int = Int(infor3.org_id!)!
+            let org_id4:Int = Int(infor4.org_id!)!
+            if org_id3 > org_id4 {
+                return ComparisonResult.orderedDescending
+            }else if org_id3 < org_id4 {
+                return ComparisonResult.orderedAscending
+            }else{
+                return ComparisonResult.orderedSame
+            }
+        }
+        for index in 0..<list.count {
+            let model:CompanyListInfoMode = list[index] as! CompanyListInfoMode
+            print(model.org_id)
         }
     }
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
